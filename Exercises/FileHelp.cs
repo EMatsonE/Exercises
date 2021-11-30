ffd78552ebc6b4b13afeb30d90685786f014b1a2
Em Ma <emilymtsn3@gmail.com>
2021-11-29 19:18:30 -0600

    Add project files.

---
+++ Exercises/FileHelp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Exercises
+{
+    public class FileHelp
+    {
+        public static void WriteToFile(string fileLocation, List<int> noombars)
+        {
+            StreamWriter writeOnFile = new StreamWriter(fileLocation, true);
+
+            //should take in ints and convert to string
+            StringBuilder builder = new StringBuilder();
+            foreach (var number in noombars)
+            {
+                builder.Append(number.ToString());
+                builder.Append("|");
+            }
+            //writes the thing
+            writeOnFile.WriteLine(builder.ToString());
+            writeOnFile.Flush();
+            writeOnFile.Close();
+        }
+        //it goes to consol and prints it there so you dont have to go to the file
+        public static string ReadFirstLine(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            string returnValue = reader.ReadLine();
+            if (string.IsNullOrEmpty(returnValue))
+            {
+                return "no value";
+            }
+            else
+            {
+                return returnValue;
+            }
+
+        }
+    }
+}
---
+++ Exercises/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> noombars = new List<int>() { 2,435,756,1234,8756};
+            noombars.Add(543);
+            noombars.Add(7654);
+            noombars.Add(2754);
+            string fileLocation = @"c:\code\ints.txt";
+            FileHelp.WriteToFile(fileLocation, noombars);
+            string firstLine = FileHelp.ReadFirstLine(fileLocation);
+            Console.WriteLine(firstLine);
+        }
+    }
+}
